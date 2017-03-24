ea85401e067fdf4de589689871fdc7f2d1572ecd
maidonghu <maidonghu@users.noreply.github.com>
2017-03-23 20:26:41 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+read -s -p "Enter Root Password: "  pswd
+echo -e "$pswd\n$pswd" | passwd
 adduser mike
-passwd mike
+read -s -p "Enter mike's new Password: "  pswd
+echo -e "$pswd\n$pswd" | passwd mike
+unset pswd
 gpasswd -a mike wheel
- create ssh key file for mike
-su - mike
-mkdir .ssh
-chmod 700 .ssh
-chmod 600 .ssh/authorized_keys
-exit
+
+mkdir /home/mike/.ssh
+yum install wget unzip -y
+wget https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/authorized_keys
+cat ./authorized_keys >> /home/mike/.ssh/authorized_keys
+chown mike:mike /home/mike -R
+
+yum update -y
+timedatectl set-timezone America/Vancouver
