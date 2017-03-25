10fcc967d96054079c10fba682476129ae619254
maidonghu <maidonghu@users.noreply.github.com>
2017-03-25 06:20:42 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,6 +19,8 @@ systemctl enable ntpd
 systemctl start ntpd
 
 sed -i 's/#Port 22/Port 50009/' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 systemctl restart sshd
 
 systemctl enable firewalld
