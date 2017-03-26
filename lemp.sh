5159b208a4c9bdef5462a544e65aa1c127f2a5da
maidonghu <maidonghu@users.noreply.github.com>
2017-03-26 03:39:11 -0700

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -5,8 +5,8 @@ sudo yum install  mariadb-server -y
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
 sudo mv /root/mysql_secure_installation.sql ./
-sudo mysql -sfu root < "mysql_secure_installation.sql"
-rm -f mysql_secure_installation.sql
+sudo mysql -sfu root < "/mysql_secure_installation.sql"
+sudo rm -f /mysql_secure_installation.sql
 sudo wget -O /etc/my.cnf.d/server.cnf https://raw.githubusercontent.com/maidonghu/centos-ln/master/server.cnf
 sudo systemctl restart mariadb
 
