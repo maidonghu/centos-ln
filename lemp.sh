99caf9a76894d1daf6a812b2e86bce89b7dc6601
maidonghu <maidonghu@users.noreply.github.com>
2017-04-11 06:29:54 -0700

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -45,12 +45,12 @@ sudo systemctl restart php-fpm
 #gpgcheck=0
 #enabled=1' | sudo tee /etc/yum.repos.d/nginx.repo
 #sudo yum install nginx -y
-#sudo rpm -ivh https://raw.githubusercontent.com/maidonghu/centos-ln/master/nginx-1.11.13-1.el7.centos.ngx.x86_64.rpm
+sudo rpm -ivh https://raw.githubusercontent.com/maidonghu/centos-ln/master/nginx-1.11.13-1.el7.centos.ngx.x86_64.rpm
 
 #sudo yum install https://repo.aerisnetwork.com/stable/centos/7/x86_64/aeris-release-1.0-4.el7.noarch.rpm -y
 #sudo yum install nginx-more -y
-cd /etc/yum.repos.d
-sudo wget https://repo.codeit.guru/codeit.mainline.el`rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release)`.repo
+#cd /etc/yum.repos.d
+#sudo wget https://repo.codeit.guru/codeit.mainline.el`rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release)`.repo
 sudo yum install nginx -y
 
 sudo systemctl disable httpd
