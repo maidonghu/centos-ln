b3ba5e67bb48ecadd135e8d48da57357f4c8cf29
maidonghu <maidonghu@users.noreply.github.com>
2017-05-25 08:33:34 -0700

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -10,7 +10,7 @@ sudo yum install epel-release -y
 sudo yum localinstall --nogpgcheck -y https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
 sudo yum install -y ffmpeg jemalloc
 sudo yum install -y c-ares
-sudo rpm -ivh https://raw.githubusercontent.com/maidonghu/centos-ln/master/aria2-1.31.0-2.el7.centos.x86_64.rpm
+sudo rpm -ivh https://raw.githubusercontent.com/maidonghu/centos-ln/master/aria2-1.32.0-1.el7.centos.x86_64.rpm
 
 cd /mnt/volume-nyc1-01
 sudo mkdir -p Downloads
