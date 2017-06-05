942a09711c473bbc21a5010e5cc6ad0c7328ef06
maidonghu <maidonghu@users.noreply.github.com>
2017-06-05 01:51:55 -0700

    Update nginx-ssl.sh

--- nginx-ssl.sh
+++ nginx-ssl.sh
@@ -3,15 +3,15 @@ yum -y groupinstall 'Development Tools'
 yum -y install wget openssl-devel libxml2-devel libxslt-devel jemalloc-devel gd-devel perl-ExtUtils-Embed GeoIP-devel rpmdevtools
 
 OPENSSL="openssl-1.0.2k"
-NGINX_VERSION="1.12.0-1"
-NJS_VERSION="1.12.0.0.1.10-1"
+NGINX_VERSION="1.13.1-1"
+NJS_VERSION="1.13.1.0.1.10-1"
 
-rpm -ivh http://nginx.org/packages/centos/7/SRPMS/nginx-$NGINX_VERSION.el7.ngx.src.rpm
-rpm -ivh http://nginx.org/packages/centos/7/SRPMS/nginx-module-geoip-$NGINX_VERSION.el7.ngx.src.rpm
-rpm -ivh http://nginx.org/packages/centos/7/SRPMS/nginx-module-image-filter-$NGINX_VERSION.el7.ngx.src.rpm
-rpm -ivh http://nginx.org/packages/centos/7/SRPMS/nginx-module-njs-$NJS_VERSION.el7.ngx.src.rpm
-rpm -ivh http://nginx.org/packages/centos/7/SRPMS/nginx-module-perl-$NGINX_VERSION.el7.ngx.src.rpm
-rpm -ivh http://nginx.org/packages/centos/7/SRPMS/nginx-module-xslt-$NGINX_VERSION.el7.ngx.src.rpm
+rpm -ivh http://nginx.org/packages/mainline/centos/7/SRPMS/nginx-$NGINX_VERSION.el7.ngx.src.rpm
+rpm -ivh http://nginx.org/packages/mainline/centos/7/SRPMS/nginx-module-geoip-$NGINX_VERSION.el7.ngx.src.rpm
+rpm -ivh http://nginx.org/packages/mainline/centos/7/SRPMS/nginx-module-image-filter-$NGINX_VERSION.el7.ngx.src.rpm
+rpm -ivh http://nginx.org/packages/mainline/centos/7/SRPMS/nginx-module-njs-$NJS_VERSION.el7.ngx.src.rpm
+rpm -ivh http://nginx.org/packages/mainline/centos/7/SRPMS/nginx-module-perl-$NGINX_VERSION.el7.ngx.src.rpm
+rpm -ivh http://nginx.org/packages/mainline/centos/7/SRPMS/nginx-module-xslt-$NGINX_VERSION.el7.ngx.src.rpm
 
 sed -i "/Source12: .*/a Source100: https://raw.githubusercontent.com/maidonghu/centos-ln/master/$OPENSSL.tar.gz" /root/rpmbuild/SPECS/nginx.spec
 sed -i "s|--with-http_ssl_module|--with-http_ssl_module --with-openssl=$OPENSSL|g" /root/rpmbuild/SPECS/nginx.spec
